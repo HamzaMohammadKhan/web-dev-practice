1dc9fa6e00ac9dc2e8a6989d07c962e736a62ee6
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-09-13 21:36:16 +0500

    JS

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("HELLO");
+console.log("Hamza here");
+console.log("Iam a software emgineering ");
+console.log("Passout 2019");
+console.log("Work hard to achieve your goals");
+console.log("Yooooo");
+
+//variables
+
+var variableName = 'Welcome';
+console.log(variableName);
+var newName = 'Yo';
+console.log(newName);
+var x = 3;
+var y = 4;
+var z = x+y;
+console.log(z);
+var o=2.56;
+var p = 2;
+console.log(o+p);
